1caa7938e9c1efa7f85101194e545e57d5ba4c68
gabriel <gbordesc@uft.edu>
2020-12-25 21:11:29 -0300

    parte de repo

---
+++ main.py
@@ -0,0 +1,12 @@
+from borracho import BorrachoTradicional
+from campo import Campo
+from coordenada import Coordenada
+
+
+def main(distancias,intentos,tipo_de_Borracho):
+
+
+if __name__ == '__main__':
+    distancias_de_caminata=[10,100,1000,10000]
+    numero_de_intentos=100 #numero de cantidades que se hara la simulacion
+    main(distancias_de_caminata,numero_de_intentos,BorrachoTradicional)
